b29a08a7fed59893b7461ee88ac500527bd91f3e
Anthony Flores <antone@main-10-230-16-147.tamulink.tamu.edu>
2018-10-16 14:19:36 -0500

    total requests, months,  4xx, and 3xx

--- TCMG476.py
+++ TCMG476.py
@@ -21,68 +21,55 @@ while line:
   #print(line)
   line = fh.readline()
 
-  #print(line)
 fh.close()
-
 things = []
-ERRORS = []
 
 # Let's say we're counting the number of times that a particular filename appears in a log file
 for line in open(FILE_NAME):
 
   # Use the Regex module to split out the filename from the line
-  pieces = re.split(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*", line)
-  #print(pieces)
+  pieces = line.split(" ")
   things.append(pieces)
 
-  regex = re.compile(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*")
-
-  # Call the split() method to get all the capture groups put in a list
-  parts = regex.split(line)
-
-  # Let's see what the regex grabbed...
-  print(parts)
-
-  # Sanity check the line -- there should be 7 elements in the list (remember that index 0 has the whole string)
-  if not parts or len(parts) < 7:
-    print("Error parsing line! Log entry added to ERRORS[] list...")
-    ERRORS.append(line)
-  else:
-    print("\n")
-
-number = 0
-size = len(things)
-counter = 0
-while(counter < size):
-  if things[counter][6][0] == "4":
-    number += 1
-  counter += 1
-
-print(number)
-
-
-
-
+lineTotal = 0
 for line in things:
-    if line[6][0] == "4":
-      number += 1
-print("400 % = " + str(number))
+    lineTotal += 1
+
+print("Total requests " + str(lineTotal))
+
+count400 = 0
+for line in things:
+    if len(line) > 9:
+        if line[8][0] == '4':
+            count400 += 1
+print ("4xx requests " + str(count400))
+
+count300 = 0
+for line in things:
+    if len(line) > 9:
+        if line[8][0] == '3':
+            count300 +=1
+print("3xx requests " + str(count300))
+
+countOct = 0
+for line in things:
+    if len(line) > 9:
+        if line[3][4] == 'O':
+            countOct +=1
+print("October requests " + str(countOct))
+
+countNov = 0
+for line in things:
+    if len(line) > 9:
+        if line[3][4] == 'N':
+            countNov +=1
+print("November requests " + str(countNov))
 
 
-  # Let's further say that we can get the filename part at the 4th list element
-print("Length of array")
-print (len(things))
-print("\n")
-
-print("First line")
-print(things[0])
-print(things[0][6][0])
-print("\n")
-
-print("Last Line")
-print(things[43538])
-
-
+oct = "oct"
+nov = "nov"
+countMD = [[oct,nov], [1,2,3]]
+print(countMD[0],[0])
 
 
 '''
