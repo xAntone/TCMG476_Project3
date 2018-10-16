cd2319c393b738629c1751d11c8b6872c13dd803
Anthony Flores <antone@main-10-230-16-147.tamulink.tamu.edu>
2018-10-16 14:13:53 -0500

    total requests, months,  4xx, and 3xx

--- TCMG476.py
+++ TCMG476.py
@@ -25,14 +25,79 @@ while line:
 fh.close()
 
 things = []
+ERRORS = []
 
 # Let's say we're counting the number of times that a particular filename appears in a log file
 for line in open(FILE_NAME):
 
   # Use the Regex module to split out the filename from the line
   pieces = re.split(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*", line)
+  #print(pieces)
   things.append(pieces)
 
+  regex = re.compile(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*")
+
+  # Call the split() method to get all the capture groups put in a list
+  parts = regex.split(line)
+
+  # Let's see what the regex grabbed...
+  print(parts)
+
+  # Sanity check the line -- there should be 7 elements in the list (remember that index 0 has the whole string)
+  if not parts or len(parts) < 7:
+    print("Error parsing line! Log entry added to ERRORS[] list...")
+    ERRORS.append(line)
+  else:
+    print("\n")
+
+number = 0
+size = len(things)
+counter = 0
+while(counter < size):
+  if things[counter][6][0] == "4":
+    number += 1
+  counter += 1
+
+print(number)
+
+
+
+
+for line in things:
+    if line[6][0] == "4":
+      number += 1
+print("400 % = " + str(number))
+
+
   # Let's further say that we can get the filename part at the 4th list element
-print(things[0][2])
+print("Length of array")
+print (len(things))
+print("\n")
+
+print("First line")
 print(things[0])
+print(things[0][6][0])
+print("\n")
+
+print("Last Line")
+print(things[43538])
+
+
+
+
+'''
+# print("\nErrors: " + str(len(ERRORS)))
+#print(ERRORS)
+
+count3 = 0
+count4 = 0
+for i in range(0,len(things)):
+  print(i)
+  if str(things[i][6]).startswith("4"):
+    count4 += 1
+  elif str(things[i][6]).startswith("3"):
+    count3 += 1
+
+print(count4)
+print(count3)
+'''
